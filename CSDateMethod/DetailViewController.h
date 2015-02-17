726b33142d5be84764f617291f812ee21ff15068
Cloud <clouds.sung@gmail.com>
2015-02-17 10:18:09 +0800

    add DetailViewContrller

---
+++ CSDateMethod/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  CSDateMethod
+//
+//  Created by SungHsiang Yu on 2015/2/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UITableViewController
+
+@end
